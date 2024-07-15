5aa5b07d62eab9c6f2b485c20de612d857e2f275
Pyae Sone Nyein <pyaesonenyein1996@gmail.com>
2024-07-15 16:33:41 +0630

    init 2

--- src/App.js
+++ src/App.js
@@ -26,6 +26,9 @@ export default function App() {
   );
 }
 
+// console.log(<DifferentContent test={2}/>);
+// console.log(content.at(2));
+
 function Tabbed({ content }) {
   const [activeTab, setActiveTab] = useState(0);
 
@@ -39,7 +42,7 @@ function Tabbed({ content }) {
       </div>
 
       {activeTab <= 2 ? (
-        <TabContent item={content.at(activeTab)} />
+        <TabContent item={content.at(activeTab)} key={content.at(activeTab).summary} />
       ) : (
         <DifferentContent />
       )}
